3c40f6e8542229934605e354faf16bc073384a60
Aaron Anil <aaronanilv1007@gmail.com>
2025-11-22 09:30:04 +0530

    Back to the original test

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -2,10 +2,10 @@ package com.example.calculator;
 
 public class Calculator {
     public int add(int a, int b) {
-        return a - b;
+        return a + b;
     }
     public int subtract(int a, int b) {
-        return a + b;
+        return a - b;
     }
     public int multiply(int a, int b) {
         return a * b;
